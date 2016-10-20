e58cbd6c0701de5067475230565b6fc6902cbaf4
Tulir Asokan <tulir@maunium.net>
2016-10-20 09:34:59 +0300

    Switch to SCSS and make formatting changes in files

--- client/main.js
+++ client/main.js
@@ -4,13 +4,13 @@ import { ReactiveVar } from 'meteor/reactive-var'
 import './main.html'
 
 Template.sidebar.onCreated(function() {
-  this.apiname = new ReactiveVar("An API")
+	this.apiname = new ReactiveVar("An API")
 })
 
 Template.sidebar.helpers({
-  apiname() {
-    return Template.instance().apiname.get()
-  },
+	apiname() {
+		return Template.instance().apiname.get()
+	},
 })
 
 Template.container.onCreated(function() {
@@ -18,35 +18,35 @@ Template.container.onCreated(function() {
 })
 
 Template.container.helpers({
-    apiEditor() {
-        return "apied-info"
-    }
+	apiEditor() {
+		return "apied-info"
+	}
 })
 
 Template["apied-info"].events = {
-    "submit form": function(event, template) {
-        event.preventDefault()
-        $("#temporaryOutput").html(JSON.stringify($("#api-basic-data").serializeObject(), null, "    "))
-    }
+	"submit form": function(event, template) {
+		event.preventDefault()
+		$("#temporaryOutput").html(JSON.stringify($("#api-basic-data").serializeObject(), null, "	"))
+	}
 }
 
 $.assignValue = function(obj, keyPath, value) {
-    lastKeyIndex = keyPath.length-1
-    for (var i = 0; i < lastKeyIndex; ++ i) {
-        key = keyPath[i]
-        if (!(key in obj)) {
-            obj[key] = {}
-        }
-        obj = obj[key]
-    }
-    obj[keyPath[lastKeyIndex]] = value
+	lastKeyIndex = keyPath.length-1
+	for (var i = 0; i < lastKeyIndex; ++ i) {
+		key = keyPath[i]
+		if (!(key in obj)) {
+			obj[key] = {}
+		}
+		obj = obj[key]
+	}
+	obj[keyPath[lastKeyIndex]] = value
 }
 
 $.fn.serializeObject = function() {
-    let obj = {}
-    let arr = this.serializeArray()
-    $.each(arr, function() {
-        $.assignValue(obj, this.name.split("."), this.value)
-    })
-    return obj
+	let obj = {}
+	let arr = this.serializeArray()
+	$.each(arr, function() {
+		$.assignValue(obj, this.name.split("."), this.value)
+	})
+	return obj
 }
