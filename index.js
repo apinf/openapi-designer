13bd384809640292cae972507c7714e07dea42e6
Tulir Asokan <tulir@maunium.net>
2016-10-26 16:16:22 +0300

    Downgrade to Bootstrap 3 and add initial schema

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 $("#form").alpaca({
 	"schema": {
-		"title": "What do you think of Alpaca?",
+		"title": "Open API designer",
 		"type": "object",
 		"properties": {
-			"name": {
+			"title": {
 				"type": "string",
-				"title": "Name"
+				"title": "Title"
 			},
-			"ranking": {
+			"version": {
 				"type": "string",
-				"title": "Ranking",
-				"enum": ['excellent', 'not too shabby', 'alpaca built my hotrod']
+				"title": "Version",
+			},
+			"license": {
+				"title": "License",
+				"type": "object",
+				"properties": {
+					"name": {
+						"type": "string",
+						"title": "Name"
+					},
+					"url": {
+						"type": "string",
+						"title": "URL"
+					},
+				}
+			},
+			"host": {
+				"type": "string",
+				"title": "Host"
+			},
+			"basePath": {
+				"type": "string",
+				"title": "Base path"
+			},
+			"schemes": {
+				"description": "Schemes",
+				"type": "array",
+				"items": {
+					"title": "Scheme",
+					"type": "string"
+				},
+				"minItems": 1
 			}
 		}
 	}
