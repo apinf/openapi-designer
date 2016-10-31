7336ee31ddd754a88f4a3f5c8ddbab6c7922f38a
Tulir Asokan <tulir@maunium.net>
2016-10-31 15:26:34 +0200

    Add custom validators for host and basePath

--- index.js
+++ index.js
@@ -75,5 +75,68 @@ $("#form").alpaca({
 				"minItems": 1
 			}
 		}
+	},
+	options: {
+		fields: {
+			info: {
+				fields: {
+					host: {
+						validator: function(callback) {
+							// TODO choose a validator below (or find a library)
+
+							/* Regex validator
+								+ Short
+								- Generic error message
+								- No unicode support
+								+ Similar to Alpaca validators (they don't support unicode either)
+
+							if (/^([A-Za-z0-9]+\.)*([A-Za-z0-9]+){1}(:[0-9]+)?$/.test(callback)) {
+								callback({status: false, message: "Invalid hostname"})
+							} else {
+								callback({status: true})
+							}
+							*/
+
+							/* Javascript validator
+								+ Proper error messages
+								+ Unicode support
+								- Long
+
+							let host = this.getValue()
+							if (host.indexOf(":") !== -1) {
+								let parts = host.split(":")
+								host = parts[0]
+								// Strict string -> number conversion
+								let port = (+parts[1])
+								if (isNaN(port)) {
+									callback({status: false, message: "Invalid port"})
+								} else if (port <= 0) {
+									callback({status: false, message: "Port too small"})
+								} else if (port >= 65536) {
+									callback({status: false, message: "Port too large"})
+								}
+							}
+
+							for (char of host) {
+								if (isNaN(char) && char !== "." &&
+									char.toLowerCase() === char.toUpperCase()) {
+									callback({status: false, message: "Invalid character in hostname"})
+									return
+								}
+							}
+							callback({status: true})
+							*/
+						}
+					},
+					basePath: {
+						validator: function(callback) {
+							if (!this.getValue().startsWith("/")) {
+								this.setValue("/" + this.getValue())
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 })
