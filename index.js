250b175eab47121ec92a6179f10daaeff19984c4
Tulir Asokan <tulir@maunium.net>
2016-11-03 10:56:39 +0200

    Move schema to external file and precompile SRL expressions

--- index.js
+++ index.js
@@ -1,102 +1,27 @@
+const hostFieldValidator = new SRL('\
+	begin with capture (                                     \
+		capture (                                            \
+			any of (digit, letter, one of "-") once or more, \
+			literally "." once                               \
+		) never or more,                                     \
+		any of (digit, letter, one of "-") once or more      \
+	) once,                                                  \
+	capture (                                                \
+		literally ":" once,                                  \
+		digit once or more                                   \
+	) optional,                                              \
+	must end, case insensitive                               \
+')
+
 $("#form").alpaca({
-	schema: {
-		"title": "Open API designer",
-		"type": "object",
-		"properties": {
-			"info": {
-				"type": "object",
-				"title": "Info",
-				"properties": {
-					"title": {
-						"type": "string",
-						"title": "Title"
-					},
-					"version": {
-						"type": "string",
-						"title": "Version",
-					},
-					"termsOfService": {
-						"type": "string",
-						"title": "Terms of Service",
-					},
-					"contact": {
-						"title": "Contact",
-						"type": "object",
-						"properties": {
-							"name": {
-								"type": "string",
-								"title": "Name"
-							},
-							"url": {
-								"type": "string",
-								"format": "url",
-								"title": "URL"
-							},
-							"email": {
-								"type": "string",
-								"format": "email",
-								"title": "Email"
-							}
-						}
-					},
-					"license": {
-						"title": "License",
-						"type": "object",
-						"properties": {
-							"name": {
-								"type": "string",
-								"title": "Name"
-							},
-							"url": {
-								"type": "string",
-								"format": "url",
-								"title": "URL"
-							},
-						}
-					},
-					"host": {
-						"type": "string",
-						"title": "Host"
-					},
-					"basePath": {
-						"type": "string",
-						"title": "Base path"
-					}
-				}
-			},
-			"schemes": {
-				"title": "Schemes",
-				"type": "array",
-				"items": {
-					"title": "Scheme",
-					"type": "string",
-					"enum": ["http", "https", "ws", "wss"]
-				},
-				"minItems": 1
-			}
-		}
-	},
+	schemaSource: "/schema.json",
 	options: {
 		fields: {
 			info: {
 				fields: {
 					host: {
 						validator: function(callback) {
-							let query = new SRL('\
-								begin with capture (                                     \
-								    capture (                                            \
-								        any of (digit, letter, one of "-") once or more, \
-								        literally "." once                               \
-								    ) never or more,                                     \
-								    any of (digit, letter, one of "-") once or more      \
-								) once,                                                  \
-								capture (                                                \
-								    literally ":" once,                                  \
-								    digit once or more                                   \
-								) optional,                                              \
-								must end, case insensitive                               \
-							')
-							if (query.isMatching(this.getValue())) {
+							if (hostFieldValidator.isMatching(this.getValue())) {
 								callback({status: true})
 							} else {
 								callback({status: false, message: "Invalid hostname e.g. host.example.com:80"})
