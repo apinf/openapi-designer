2e82d920820d3f242cbf28043ca418cfe407848e
Tulir Asokan <tulir@maunium.net>
2016-11-07 15:13:07 +0200

    Add browserify for packaging

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    entry: "./node_modules/srl/lib/SRL.js",
-    output: {
-        path: __dirname,
-        filename: "./node_modules/srl/dist/srl.js",
-		library: "SRL",
-		libraryTarget: "var"
-    }
-};
