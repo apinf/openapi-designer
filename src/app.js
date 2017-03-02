2475466f197959fc79f84b7bbcf5b31656b4ba19
Tulir Asokan <tulir@maunium.net>
2017-03-02 13:38:15 +0200

    Move schemas to schema directory

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import 'aurelia-mdl';
 import {componentHandler} from 'encapsulated-mdl';
 import {fieldTypes} from './resources/fieldtypes';
+import {header} from './schema/header';
+import {mime} from './schema/mime';
 
 export class App {
   formSchema = {};
@@ -8,6 +10,9 @@ export class App {
   constructor() {
     // Allow access from browser console
     window.$oai = this;
+
+    this.formSchema.header = this.parseJSON('header', header);
+    this.formSchema.mime = this.parseJSON('mime', mime);
   }
 
   parseJSON(id, obj) {
@@ -22,58 +27,5 @@ export class App {
   attached() {
     // Activate Material Design Lite
     componentHandler.upgradeElement(document.getElementById('container'));
-  }
-
-  bind() {
-    // Temporary hardcoded form data
-    this.formSchema.header = this.parseJSON('header', {
-      type: 'object',
-      children: {
-        info: {
-          type: 'object',
-          children: {
-            title: {
-              type: 'text',
-              columns: 5
-            },
-            version: {
-              type: 'text',
-              columns: 3
-            },
-            description: {
-              type: 'textarea'
-            },
-            termsofservice: {
-              type: 'text',
-              label: 'Terms of Service'
-            }
-          }
-        },
-        contact: {
-          type: 'object',
-          children: {
-            name: {
-              type: 'text'
-            },
-            email: {
-              type: 'text'
-            },
-            url: {
-              type: 'text',
-              label: 'URL'
-            }
-          }
-        }
-      }
-    });
-    this.formSchema.mime = this.parseJSON('mime', {
-      type: 'array',
-      label: 'Array Test',
-      item: {
-        id: 'array-elem',
-        type: 'text',
-        labelFormat: 'Mui'
-      }
-    });
   }
 }
---
+++ src/schema/mime.js
@@ -0,0 +1,9 @@
+export const mime = {
+  'type': 'array',
+  'label': 'Array Test',
+  'item': {
+    'id': 'array-elem',
+    'type': 'text',
+    'labelFormat': 'Mui'
+  }
+};
