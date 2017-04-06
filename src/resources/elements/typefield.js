32c8dac2203e95b4da5aea406c86ed1bea108703
Tulir Asokan <tulir@maunium.net>
2017-04-06 16:33:51 +0300

    Add JSDocs and @containerless to Typefield

--- src/resources/elements/typefield.js
+++ src/resources/elements/typefield.js
@@ -1,13 +1,50 @@
-import {bindable} from 'aurelia-framework';
+import {containerless, bindable} from 'aurelia-framework';
 import {Field} from './abstract/field';
 import {parseJSON} from '../jsonparser';
 
+/**
+ * Typefield is a {@link Field} that shows different child forms depending on
+ * the type the user chooses.
+ *
+ * This allows for theoretically infinite nesting because it uses lazy
+ * evaluation and supports $refs in type options.
+ */
+@containerless
 export class Typefield extends Field {
+  /**
+   * The type that is currently selected.
+   * @type {String}
+   */
   @bindable selectedType = '';
+  /**
+   * The key to set the value of the child field to in the output of this field.
+   * @type {String}
+   */
   valueKey = '';
+  /**
+   * Whether or not to show the chosen type in the value of this field.
+   * @type {Boolean}
+   */
   showType = true;
+  /**
+   * Whether or not to copy the value over to the new child when switching types.
+   *
+   * N.B. Absolutely nothing is done to ensure the data ends up in the right
+   * place. Only use this if most of the fields in different types are in nearly
+   * the same places.
+   * @type {Boolean}
+   */
   copyValue = false;
+  /**
+   * The current child field.
+   * @type {Field}
+   * @private
+   */
   child = undefined;
+  /**
+   * The schemas for the available types.
+   * @type {Object}
+   */
   types = {};
 
   /** @inheritdoc */
@@ -27,6 +64,10 @@ export class Typefield extends Field {
     return super.init(id, args);
   }
 
+  /**
+   * Called by Aurelia when the selected type changes (e.g. from the dropdown)
+   * @param {String} newType The new type.
+   */
   selectedTypeChanged(newType) {
     const newSchema = this.types[newType];
     const value = this.copyValue ? this.getValue() : undefined;
@@ -45,6 +86,22 @@ export class Typefield extends Field {
     }
   }
 
+  /**
+   * Get the value of this field.
+   *
+   * If the child is undefined, this will return undefined.
+   * If {@link #showType} is {@linkplain false}, the raw value of the child is
+   * returned.
+   * If {@link #showType} is {@linkplain true} and {@link #valueKey} is defined
+   * OR the raw value of the child is not an object, the raw value of the child
+   * will be placed in an object with {@link #valueKey} or {@linkplain value}
+   * as the key.
+   * If {@link #showType} is {@linkplain true}, {@link #valueKey} is not
+   * defined and the raw value of the child is an object, the type will simply
+   * be added to the raw value of the child.
+   *
+   * @return {Object} The value.
+   */
   getValue() {
     if (!this.child) {
       return undefined;
@@ -67,11 +124,13 @@ export class Typefield extends Field {
   }
 
   setValue(value) {
-    if (typeof value === 'object' && !Array.isArray(value)) {
+    if (!this.showType) {
+      this.child.setValue(value);
+    } else if (typeof value === 'object' && !Array.isArray(value)) {
       delete value.type;
       this.child.setValue(value);
     } else {
-      this.child.setValue(value[this.valueKey]);
+      this.child.setValue(value[this.valueKey || 'value']);
     }
   }
 
@@ -83,6 +142,10 @@ export class Typefield extends Field {
     return this.selectedType;
   }
 
+  /**
+   * Get all the possible type names.
+   * @return {String[]} The names of the possible types.
+   */
   get possibleTypes() {
     return Object.keys(this.types);
   }
