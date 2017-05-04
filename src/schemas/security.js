74dd276f5241e6d0d8cc049623e01af53f4e5c45
Tulir Asokan <tulir@maunium.net>
2017-05-04 10:42:11 +0300

    Implement security definition form

--- src/schemas/security.js
+++ src/schemas/security.js
@@ -1,28 +1,83 @@
 export const security = {
-  'type': 'object',
-  'children': {
-    'checkboxes': {
-      'type': 'option',
-      'format': 'checkbox',
-      'choices': [
-        'optiona',
-        {
-          'key': 'optionb',
-          'label': 'Option B'
-        },
-        'OptionC'
-      ]
-    },
-    'dropdown': {
-      'type': 'option',
-      'format': 'dropdown',
-      'choices': [
-        'The First Choice',
-        {
-          'key': 'choice2',
-          'label': 'Another choice'
+  'type': 'array',
+  'format': 'map',
+  'keyField': 'key',
+  'item': {
+    'label': 'Security definition',
+    'type': 'selectable',
+    'defaultType': 'basic',
+    'keyKey': 'key',
+    'types': {
+      'basic': {
+        'label': '',
+        'type': 'object',
+        'children': {
+          'description': {
+            'type': 'text'
+          }
         }
-      ]
+      },
+      'apiKey': {
+        'label': '',
+        'type': 'object',
+        'children': {
+          'description': {
+            'type': 'text'
+          },
+          'name': {
+            'type': 'text'
+          },
+          'in': {
+            'label': 'API key location',
+            'type': 'option',
+            'format': 'dropdown',
+            'choices': ['query', 'header']
+          }
+        }
+      },
+      'oauth2': {
+        'label': '',
+        'type': 'object',
+        'children': {
+          'description': {
+            'type': 'text'
+          },
+          'flow': {
+            'type': 'option',
+            'format': 'dropdown',
+            'choices': ['implicit', 'password', 'application', 'accessCode']
+          },
+          'authorizationUrl': {
+            'label': 'Authorization URL',
+            'type': 'text',
+            'conditions': {
+              '../flow': ['implicit', 'accessCode']
+            }
+          },
+          'tokenUrl': {
+            'label': 'Token URL',
+            'type': 'text',
+            'conditions': {
+              '../flow': ['password', 'application', 'accessCode']
+            }
+          }/*,
+          'scopes': {
+            'type': 'array',
+            'format': 'map',
+            'keyField': 'key',
+            'item': {
+              'type': 'object',
+              'keyField': 'key',
+              'children': {
+                'key': {
+                  'type': 'text'
+                },
+                'value':
+              }
+            }
+          }*/
+        }
+      }
     }
   }
 };
