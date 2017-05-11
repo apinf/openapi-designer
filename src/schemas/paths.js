854e2a3117d1bdd6b59851de7aa00266cdda27f4
Tulir Asokan <tulir@maunium.net>
2017-05-11 16:34:38 +0300

    Add part of paths form (ref #75)

--- src/schemas/paths.js
+++ src/schemas/paths.js
@@ -1,5 +1,65 @@
 export const paths = {
-  'type': 'object',
-  'children': {
+  'type': 'array',
+  'format': 'map',
+  'keyField': 'name',
+  'item': {
+    'type': 'object',
+    'label': 'Path',
+    'legendChildren': {
+      'name': {
+        'type': 'text',
+        'label': 'Enter path...'
+      }
+    },
+    'children': {
+      'title': {
+        'type': 'text',
+        'conditions': {
+          '../type': ['string', 'integer', 'number', 'array', 'object', 'null']
+        }
+      },
+      'methods': {
+        'type': 'array',
+        'format': 'map',
+        'keyField': 'method',
+        'item': {
+          'label': 'Method definition',
+          'type': 'object',
+          'legendChildren': {
+            'method': {
+              'type': 'option',
+              'format': 'dropdown',
+              'choices': [
+                {'key': 'get', 'label': 'GET'},
+                {'key': 'put', 'label': 'PUT'},
+                {'key': 'post', 'label': 'POST'},
+                {'key': 'delete', 'label': 'DELETE'},
+                {'key': 'options', 'label': 'OPTIONS'},
+                {'key': 'head', 'label': 'HEAD'},
+                {'key': 'patch', 'label': 'PATCH'}
+              ]
+            }
+          },
+          'children': {
+            'operationId': {
+              'label': 'Operation ID',
+              'type': 'text'
+            },
+            'summary': {
+              'type': 'text'
+            },
+            'description': {
+              'type': 'textarea'
+            },
+            'tags': {
+              'type': 'array',
+              'item': {
+                'type': 'text'
+              }
+            }
+          }
+        }
+      }
+    }
   }
 };
