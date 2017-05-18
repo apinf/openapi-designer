c10bc5e65938eb6828284efaf2ba789eead0f42d
Tulir Asokan <tulir@maunium.net>
2017-05-18 15:15:11 +0300

    Calculate tooltip popup location without jQuery

--- src/resources/elements/tooltip.js
+++ src/resources/elements/tooltip.js
@@ -1,20 +1,19 @@
 import {bindable} from 'aurelia-framework';
-import $ from 'jquery';
+
 export class Tooltip {
   @bindable text = '';
 
   mouseover() {
-    const textbox = $(this.textbox);
-    textbox.removeClass('hidden');
-    textbox.css({
-      // Place the tooltip "popup" below the info button so the right edges align.
-      top: textbox.offset().top + textbox.parent().height()
-    });
+    // Make the tooltip popup visible.
+    this.textbox.style.display = 'inline-block';
+    // Place the tooltip popup below the info button so the right edges align.
+    this.textbox.style.top = this.textbox.offsetTop + this.textbox.parentNode.offsetHeight + 'px';
   }
 
   mouseout() {
-    const textbox = $(this.textbox);
-    textbox.addClass('hidden');
-    textbox.css({ top: 'auto' });
+    this.textbox.style.display = 'none';
+    // Reset the top value so that the tooltip popup doesn't go lower each time
+    // you hover over the box.
+    this.textbox.style.top = 'auto';
   }
 }
