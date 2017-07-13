49f749c09efe22a18e162bb93171b2079940b069
Tulir Asokan <tulir@maunium.net>
2017-07-13 15:08:26 +0300

    Fix importing specs on IE and Edge

--- src/app.js
+++ src/app.js
@@ -146,6 +146,20 @@ export class App {
     fileInput.appendTo('body');
     fileInput.trigger('click');
 
+    // IE/Edge don't want to trigger change events, so I have to do it for them...
+    //
+    // Check that the browser is IE/Edge
+    if (!!window.StyleMedia) {
+      // Check if there is a file every 0.5 seconds
+      const interval = setInterval(() => {
+        if (fileInput[0].files[0]) {
+          // The file was found, so trigger a change event and stop the interval.
+          fileInput.trigger('change');
+          clearInterval(interval);
+        }
+      }, 500);
+    }
+
     fileInput.change(() => {
       const file = fileInput[0].files[0];
       const reader = new FileReader();
